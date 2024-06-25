ec79022852b485fb65a6fb3554429cd611736943
Yash <kudveyash@gmail.com>
2024-06-25 10:05:05 +0530

    CONFIG FILE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 function App() {
+  // console.log(process.env.REACT_APP_APPWRITE_URL); //this process in create-react-app
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
   return <div>Blog app with appwrite</div>;
 }
 
