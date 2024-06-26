a1d6995fcb40f76ad3ff537711e4174c0136d178
Yash <kudveyash@gmail.com>
2024-06-26 09:49:35 +0530

    create account functionality

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,7 +2,40 @@ import config from "../config/config";
 
 import {Client, Account, ID} from "appwrite"
 
-export class AuthService{}
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteURL)
+        .setProject(config.appwriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if (userAccount) {
+                
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+}
 
 const authService = new AuthService();
 
