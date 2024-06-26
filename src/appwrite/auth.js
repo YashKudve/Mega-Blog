2519e5ac33d9c33764400f851378a51b4909b9d1
Yash <kudveyash@gmail.com>
2024-06-26 09:29:14 +0530

    auth file updated

---
+++ src/appwrite/auth.js
@@ -0,0 +1,9 @@
+import config from "../config/config";
+
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService{}
+
+const authService = new AuthService();
+
+export default authService;
