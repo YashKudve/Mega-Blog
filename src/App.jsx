c4e118a6a94addea4797aaa1e4b766fc6cd2a0f5
Yash <kudveyash@gmail.com>
2024-06-27 10:01:15 +0530

    useEffect to handle login - logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL); //this process in create-react-app
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally();
+  }, []);
 
   return <div>Blog app with appwrite</div>;
 }
