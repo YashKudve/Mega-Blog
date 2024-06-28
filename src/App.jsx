26f56677904c799982c0567e1d8846ead2390f42
Yash <kudveyash@gmail.com>
2024-06-28 15:17:33 +0530

    UI upadate and Provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL); //this process in create-react-app
@@ -23,7 +25,17 @@ function App() {
       .finally(() => (setLoading = false));
   }, []);
 
-  return !loading ? <div className="min-h-screen">Test</div> : null;
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
