4549dcb104a31dfa3bdc35abc36f65d3e5ed5cd2
Yash <kudveyash@gmail.com>
2024-07-01 18:02:12 +0530

    login component updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom';
+import { login as authLogin } from '../store/authSlice';
+import {Button,Input, Logo} from "./index"
+import { useDispatch } from 'react-redux';
+import authService from '../appwrite/auth';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error,setError] = useState("")
+
+    const login = async(data)=>{
+      setError("")
+      try {
+        const session = await authService.login(data)
+
+        if(session){
+          const userData = await authService.getCurrentUser()
+
+          if(userData) dispatch(authLogin(userData))
+        }
+        
+      } catch (error) {
+        setError(error.message)
+      }
+    }
   return (
     <div>
       
