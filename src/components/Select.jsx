f9034300b758e8a93b193186312421a89ab6d0f2
Yash <kudveyash@gmail.com>
2024-07-01 15:54:24 +0530

    select component done

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,7 +3,7 @@ import React, {useId} from 'react'
 function Select({
     options,
     label,
-    className,
+    className="",
     ...props
 },ref) {
 
@@ -17,10 +17,17 @@ function Select({
         id={id}
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-        ></select>
+        >
+
+            {options?.map((option)=>(
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
       
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
