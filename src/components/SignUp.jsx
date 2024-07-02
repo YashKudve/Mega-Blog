5196334fb9d1fb1abbba162e68aabe542d397e70
Yash <kudveyash@gmail.com>
2024-07-02 16:44:45 +0530

    signup component updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,23 +1,46 @@
-import React,{useState} from 'react'
-import authService from '../appwrite/auth'
-import {Link,useNavigate} from 'react-router-dom'
-import { login } from '../store/authSlice'
-import {Button,Input,Logo} from "./index"
-import { useDispatch } from 'react-redux'
-import { useForm } from 'react-hook-form'
-
+import React, { useState } from "react";
+import authService from "../appwrite/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
 
-    const navigate = useNavigate()
-    const [error,setError] = useState("")
-    const dispatch = useDispatch()
-    const {register, handleSubmit} = useForm()
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login(userData));
+        }
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return (
-    <div>
-      
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
 
-export default SignUp
+  );
+};
+
+export default SignUp;
