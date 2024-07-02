a073a1c4d6b1a460ce38af7946412078732f0525
Yash <kudveyash@gmail.com>
2024-07-02 17:01:42 +0530

    minor update

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -37,6 +37,50 @@ const SignUp = () => {
             <Logo width="100%" />
           </span>
         </div>
+
+        <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
+
+        <p className='mt-2 text-center text-base text-black/60'>
+        Don't have an account?
+        <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>Sign Up</Link>
+        </p>
+        {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
+          <div className="space-y-5">
+            <Input 
+            label='Name: '
+            placeholder='Enter your name'
+            {...register("name",{
+              required:true
+            })}
+            />
+
+<Input
+        label="Email: "
+        placeholder="Enter your Email"
+        type='email'
+        {...register("email",{
+          required:true,
+          validate:{
+            matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || "Email address must be valid address",
+          }
+        })}
+        />
+
+        <Input
+        label="Password: "
+        placeholder="Enter your Password"
+        type='password'
+
+        {...register("password",{
+          required:true
+        })}
+        />
+
+          </div>
+
+        </form>
       </div>
     </div>
 
