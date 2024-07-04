bec65549e2abc687a9f2555b6f17cd9375099652
Yash <kudveyash@gmail.com>
2024-07-04 09:57:43 +0530

    updates in editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -32,15 +32,35 @@ export default function RTE({name,control,label,defaultValue=""}) {
         initialValue="default value"
         init={
             {
+                initialValue:"default value",
                 branding:false,
                 height:500,
                 menubar:true,
                 plugins:[
-                    'advlist autolink list link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table paste code help wordcount'
+                    "image",
+                    "advlist",
+                    "autolink",
+                    "list",
+                    "link",
+                    "image",
+                    "charmap",
+                    "preview",
+                    "anchor",
+                    "searchreplace",
+                    "visualblocks",
+                    "code",
+                    "fullscreen",
+                    "insertdatetime",
+                    "media",
+                    "table",
+                    "code",
+                    "help",
+                    "wordcount",
+                    "anchor"
                 ],
-                toolbar:'undo redo | formatselect | bold italic backcolor | \ alignleft aligncenter alignright alignjustify | \ bullist numlist outdent indent | removeformat | help'
+                toolbar:'undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist indent outdent | removeformat | help',
+
+                content_style:"body{font-family:Helvetica, Arial, sans-serif; font-size:14px }"
             }
         }
         onEditorChange={onChange}
