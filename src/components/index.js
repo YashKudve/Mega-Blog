7106ad147a44ae70f686d287d83d0fe49df068a6
Yash <kudveyash@gmail.com>
2024-07-04 10:07:26 +0530

    updates and configration

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,10 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
+import Button from "./Button";
+import Select from "./Select";
+import Input from "./Input";
+import RTE from "./RTE";
 
-export { Header, Footer, Container,Logo,LogoutBtn };
+
+export { Header, Footer, Container,Logo,LogoutBtn, Button, Select, Input,RTE};
