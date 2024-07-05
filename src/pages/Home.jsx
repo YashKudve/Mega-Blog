5d0fea6c931acad438da765a18ca1598c2e24602
Yash <kudveyash@gmail.com>
2024-07-05 17:07:47 +0530

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React,{useEffect,useState} from 'react'
+import appwriteService from '../config/config'
+import { Container,PostCard } from '../components'
+
+function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
