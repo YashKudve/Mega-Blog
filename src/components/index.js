b61fabfec37924b23efab05faf77088605c753b3
Yash <kudveyash@gmail.com>
2024-07-05 18:16:25 +0530

    routing and bug fix

--- src/components/index.js
+++ src/components/index.js
@@ -9,10 +9,9 @@ import Input from "./Input";
 import RTE from "./RTE";
 import SignUp from "./SignUp";
 import Login from "./Login";
-import Post from "../Post" //TODO: Check this
 import PostForm from "./PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from './AuthLayout'
 
 
-export { Header, Footer, Container,Logo,LogoutBtn, Button, Select, Input,RTE,SignUp, Login, Post, PostCard, PostForm, AuthLayout};
+export { Header, Footer, Container,Logo,LogoutBtn, Button, Select, Input,RTE,SignUp, Login, PostCard, PostForm, AuthLayout};
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login, SignUp } from "./components/index.js";
 import AllPosts from "./pages/AllPosts.jsx";
@@ -75,7 +75,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
