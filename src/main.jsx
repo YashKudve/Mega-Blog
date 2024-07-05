399fe126e0ad5c6da5707831669f869559c417db
Yash <kudveyash@gmail.com>
2024-07-05 17:59:29 +0530

    routing pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
