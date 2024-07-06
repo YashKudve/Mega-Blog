a6121c1453649b2b9673efb1fb9da829c621e152
Yash <kudveyash@gmail.com>
2024-07-06 15:58:56 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL); //this process in create-react-app
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log("Appwrite URL: ",import.meta.env.VITE_APPWRITE_URL);
+  console.log("Project ID: ",import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  console.log("Database ID",import.meta.env.VITE_APPWRITE_DATABASE_ID);
+  console.log("Collection ID",import.meta.env.VITE_APPWRITE_COLLECTION_ID);
+  console.log("Bucket ID",import.meta.env.VITE_APPWRITE_BUCKET_ID);
 
   let [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
